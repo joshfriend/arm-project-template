587d102ecdeb90ff68b81e99562cf53fa9411399
Josh Friend <josh@fueledbycaffeine.com>
2014-02-03 17:40:00 -0500

    added typedef of nvic_handler_t to make things look neater

--- startup_gcc.c
+++ startup_gcc.c
@@ -1,3 +1,24 @@
+/*
+ * Copyright (c) 2014 Josh Friend
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
 
 #include <stdint.h>
 #include <inc/hw_types.h>
@@ -19,15 +40,17 @@ extern uint32_t _data;
 extern uint32_t _edata;
 extern uint32_t _bss;
 extern uint32_t _ebss;
+extern uint32_t _stack_top;
 
-static unsigned long pulStack[64];
+// Typedef for exception handler function
+typedef void (*nvic_handler_t)(void);
 
 // NVIC exception table
 __attribute__ ((section(".nvic_table")))
-void(* nvic_table[])(void) = {
+nvic_handler_t nvic_table[] = {
     // Stack pointer starts at top of RAM
-    // (void (*)(void)) (0x20000000 + 64*1024),  // 64k of RAM starting at 0x20000000
-    (void (*)(void))((unsigned long)pulStack + sizeof(pulStack)),
+    // Stack size is defined by linker script
+    (nvic_handler_t)&_stack_top,
 
     // Fixed priority exceptions
     reset_handler,          // code entry point                 1
